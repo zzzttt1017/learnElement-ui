f70a48c7ea409be5c4418e9c4884352ca24456e2
someCat <zt@somecat.cn>
2018-05-03 16:45:23 +0800

    配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +19,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body{
+  padding: 0;
+  margin: 0
+}
+
 </style>
